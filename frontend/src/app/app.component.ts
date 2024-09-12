c8804e9a8c25079a7c500e67283cbf77985c43c3
Sagar Shivappa <62869763+sagar-shivappa@users.noreply.github.com>
2024-09-12 15:58:36 +0530

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "./services/http-service.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "ekart-app";
+  message$: any = "";
+  constructor(private httpService: HttpService) {}
+  ngOnInit() {
+    this.message$ = this.httpService.message$;
+  }
 }
