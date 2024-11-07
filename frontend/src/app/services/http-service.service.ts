005404e1795c6bbb063465a3eea3b4eff79fee9e
Sagar R Shivappa <sagar.rs@globant.com>
2024-11-07 18:15:35 +0530

    updated solution 7th nov

--- frontend/src/app/services/http-service.service.ts
+++ frontend/src/app/services/http-service.service.ts
@@ -1,43 +1,43 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { BehaviorSubject, Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class HttpService {
   //Paste URL of Live Prview of Port:8001 below
-  //  apiUrl: string = environment.apiUrl
-  apiUrl: string = "http://localhost:8002";
+  apiUrl: string = environment.apiUrl;
   message$ = new BehaviorSubject("");
 
   constructor(private http: HttpClient) {}
   login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, {
+    return this.http.post<any>(`${this.apiUrl}login`, {
       user_name: username,
       password: password,
     });
   }
   addToCart(userId: number, product_id: number): Observable<Object> {
     let url = `${this.apiUrl}`;
-    return this.http.post<any>(`${url}/cart`, {
+    return this.http.post<any>(`${url}cart`, {
       user_id: userId,
       product_id: product_id,
     });
   }
   deleteFromCart(userId: number, product_id: number): Observable<Object> {
     let url = `${this.apiUrl}`;
-    return this.http.delete<any>(`${url}/cart/${userId}/${product_id}`);
+    return this.http.delete<any>(`${url}cart/${userId}/${product_id}`);
   }
 
   cartByUserId(userId: number): Observable<any> {
     let url = `${this.apiUrl}`;
-    return this.http.get<any>(`${url}/cart/${userId}`);
+    return this.http.get<any>(`${url}cart/${userId}`);
   }
 
   getProducts() {
     let url = `${this.apiUrl}`;
-    return this.http.get<any>(`${url}/products`);
+    return this.http.get<any>(`${url}products`);
   }
   setMessenger(message: any) {
     this.message$.next(message);
