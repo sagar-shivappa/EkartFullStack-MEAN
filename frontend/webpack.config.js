b083d7f5a381884a8eba025c0b8e8e29016a78a1
Sagar Shivappa <62869763+sagar-shivappa@users.noreply.github.com>
2024-11-07 18:32:24 +0530

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,22 +1,3 @@
-const { DefinePlugin } = require("webpack");
-
-const getWebSocketURL = (port) => {
-  const PROXY_URI = process.env.VSCODE_PROXY_URI;
-  if (!PROXY_URI) return "/";
-  const proxyURL = process.env.VSCODE_PROXY_URI.replace("{{port}}", port);
-  wsURL = proxyURL.replace("http", "ws");
-  return wsURL + "ng-cli-ws";
-};
-
-const getBasePath = (port) => {
-  const PROXY_URI = process.env.VSCODE_PROXY_URI;
-  if (!PROXY_URI) return "/";
-  const proxyURL = process.env.VSCODE_PROXY_URI.replace("{{port}}", port);
-  const intermediatePath = proxyURL.split("//")[1];
-  const desiredPath = intermediatePath.substring(intermediatePath.indexOf("/"));
-  return JSON.stringify(desiredPath);
-};
-
 module.exports = {
   devServer: {
     port: 8000,
@@ -24,12 +5,6 @@ module.exports = {
     allowedHosts: "all",
     client: {
       logging: "error",
-      webSocketURL: getWebSocketURL(8000),
     },
-  },
-  plugins: [
-    new DefinePlugin({
-      BASE_PATH: getBasePath(8000),
-    }),
-  ],
+  }
 };
